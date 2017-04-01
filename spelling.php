d5abadd5b6118da75d111e3d5fdcbad61f523eee
Lertluck Leela-amornsin <lertlove@hotmail.com>
2017-04-01 23:44:52 +0700

    change to use anchor spelling

--- spelling.php
+++ spelling.php
@@ -5,13 +5,12 @@
 		$q = $_GET['q'];
 
 	    echo "input = ".$q."<br>";
-	    echo "suggestion = ".doGoogleSpellingWithEmphasize($q);  //returns suggestion
+	    echo "suggestion = ".doGoogleSpellingWithAnchor($q);  //returns suggestion
 
 	}else{
 	    // no input
 	    echo "please input q parameter";
 	}
-
  
 function doGoogleSpellingWithEmphasize($q) {
  
@@ -21,7 +20,7 @@ function doGoogleSpellingWithEmphasize($q) {
     // put anchors tag in an array
     /*preg_match_all('#<a([^>]*)?>(.*)</a>#Us', $web_page, $a_array);*/
     preg_match_all('#<b>(.*)</b>#Us', $web_page, $a_array);
-
+    var_dump($a_array[0]);
     if(count($a_array[0])>0){
     	$a_link = $a_array[0][0];
     	return $a_link;
@@ -39,18 +38,13 @@ function doGoogleSpellingWithAnchor($q) {
     preg_match_all('#<a([^>]*)?>(.*)</a>#Us', $web_page, $a_array);
     
     for($j=0;$j<count($a_array[0]);$j++) {
-    	// echo($a_array[1][$j]."<br>");
+    	
     	$a_link = $a_array[0][$j];
 
         // find link with spell suggestion and return it
         if(stristr($a_link,"spell")) {
+            $a_link = strip_tags($a_link);
 
-        	$a_link = strip_tags($a_array[1][$j]);
-        	$a_link = strstr($a_link, '?q=');
-        	$a_link = strtok($a_link, '&');
-        	$a_link = str_replace("?q=","",$a_link);
-
-        	//TODO still not work for Thai, need decode escape
         	return $a_link;
         }
     }
